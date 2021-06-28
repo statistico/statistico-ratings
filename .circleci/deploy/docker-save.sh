6aba98bcf72bad80b6d8c0e4c9096b06e8427613
Joe Sweeny <joesweeny84@hotmail.com>
2021-06-28 22:03:50 +0100

    Update image reference in docker save

--- .circleci/deploy/docker-save.sh
+++ .circleci/deploy/docker-save.sh
@@ -4,4 +4,4 @@ set -e
 
 mkdir -p /tmp/workspace/docker-cache
 
-docker save -o /tmp/workspace/docker-cache/statisticoratings_console.tar statisticoratings_console:latest
+docker save -o /tmp/workspace/docker-cache/statisticoratings_console.tar statistico-ratings_console:latest
