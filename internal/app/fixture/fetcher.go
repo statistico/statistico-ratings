14cfe916731b2bc4ae0b8b8672dfd609e7d4dae5
Joe Sweeny <joesweeny84@hotmail.com>
2021-07-11 16:04:01 +0100

    Update fetcher logic and tests

--- internal/app/fixture/fetcher.go
+++ internal/app/fixture/fetcher.go
@@ -2,7 +2,6 @@ package fixture
 
 import (
 	"context"
-	"fmt"
 	"github.com/golang/protobuf/ptypes/wrappers"
 	"github.com/jonboulle/clockwork"
 	"github.com/statistico/statistico-football-data-go-grpc-client"
@@ -39,36 +38,33 @@ func (f *fetcher) ByCompetition(ctx context.Context, competitionID uint64, numSe
 }
 
 func (f *fetcher) ByDate(ctx context.Context, date time.Time) ([]*statistico.Fixture, error) {
-	var seasons []uint64
-
-	for _, competition := range f.competitions {
-		res, err := f.seasonClient.ByCompetitionID(ctx, competition, "name_desc")
-
-		if err != nil {
-			return nil, err
-		}
-
-		parsed := parseSeasons(res, 1)
-
-		if len(parsed) != 1 {
-			return nil, fmt.Errorf("no seasons returned for competition %d", competition)
-		}
-
-		seasons = append(seasons, parsed[0])
-	}
-
 	year, month, day := date.Date()
 	start := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
 	end := time.Date(year, month, day, 23, 59, 59, 0, time.UTC)
 
-	req := statistico.FixtureSearchRequest{
-		SeasonIds:            seasons,
+	request := statistico.FixtureSearchRequest{
 		DateAfter:            &wrappers.StringValue{Value: start.Format(time.RFC3339)},
 		DateBefore:           &wrappers.StringValue{Value: end.Format(time.RFC3339)},
 		Sort:                 &wrappers.StringValue{Value: "date_asc"},
 	}
 
-	return f.fixtureClient.Search(ctx, &req)
+	response, err := f.fixtureClient.Search(ctx, &request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var fixtures []*statistico.Fixture
+
+	for _, fixture := range response {
+		for _, competition := range f.competitions {
+			if fixture.Competition.Id == competition {
+				fixtures = append(fixtures, fixture)
+			}
+		}
+	}
+
+	return fixtures, nil
 }
 
 func parseSeasons(s []*statistico.Season, num int8) []uint64 {
--- internal/app/fixture/fetcher_test.go
+++ internal/app/fixture/fetcher_test.go
@@ -119,7 +119,7 @@ func TestFetcher_ByDate(t *testing.T) {
 	t.Run("fetches and returns fixtures by date", func(t *testing.T) {
 		t.Helper()
 
-		competitions := []uint64{8}
+		competitions := []uint64{8, 9}
 		fixtureClient := new(MockFixtureClient)
 		seasonClient := new(MockSeasonClient)
 		clock := clockwork.NewFakeClock()
@@ -128,13 +128,7 @@ func TestFetcher_ByDate(t *testing.T) {
 
 		fetcher := fixture.NewFetcher(competitions, fixtureClient, seasonClient, clock)
 
-		seasonClient.
-			On("ByCompetitionID", ctx, uint64(8), "name_desc").
-			Once().
-			Return(seasonResponse(), nil)
-
 		req := mock.MatchedBy(func(r *statistico.FixtureSearchRequest) bool {
-			assert.Equal(t, []uint64{3}, r.SeasonIds)
 			assert.Equal(t, "2021-07-11T00:00:00Z", r.DateAfter.GetValue())
 			assert.Equal(t, "2021-07-11T23:59:59Z", r.DateBefore.GetValue())
 			assert.Equal(t, "date_asc", r.Sort.GetValue())
@@ -151,40 +145,9 @@ func TestFetcher_ByDate(t *testing.T) {
 			t.Fatalf("Expected nil, got %s", err.Error())
 		}
 
-		assert.Equal(t, response, fixtures)
-		seasonClient.AssertExpectations(t)
-		fixtureClient.AssertExpectations(t)
-	})
-
-	t.Run("returns an error if returned by season client", func(t *testing.T) {
-		t.Helper()
-
-		competitions := []uint64{8}
-		fixtureClient := new(MockFixtureClient)
-		seasonClient := new(MockSeasonClient)
-		clock := clockwork.NewFakeClock()
-
-		ctx := context.Background()
-
-		fetcher := fixture.NewFetcher(competitions, fixtureClient, seasonClient, clock)
-
-		e := errors.New("season client error")
-
-		seasonClient.
-			On("ByCompetitionID", ctx, uint64(8), "name_desc").
-			Once().
-			Return([]*statistico.Season{}, e)
-
-		fixtureClient.AssertNotCalled(t, "Search")
-
-		_, err := fetcher.ByDate(ctx, time.Unix(1626008664, 0))
-
-		if err == nil {
-			t.Fatal("Expected error, got nil")
-		}
-
-		assert.Equal(t, "season client error", err.Error())
-		seasonClient.AssertExpectations(t)
+		assert.Equal(t, 2, len(fixtures))
+		assert.Equal(t, int64(2), fixtures[0].Id)
+		assert.Equal(t, int64(3), fixtures[1].Id)
 		fixtureClient.AssertExpectations(t)
 	})
 
@@ -202,13 +165,7 @@ func TestFetcher_ByDate(t *testing.T) {
 
 		e := errors.New("fixture client error")
 
-		seasonClient.
-			On("ByCompetitionID", ctx, uint64(8), "name_desc").
-			Once().
-			Return(seasonResponse(), nil)
-
 		req := mock.MatchedBy(func(r *statistico.FixtureSearchRequest) bool {
-			assert.Equal(t, []uint64{3}, r.SeasonIds)
 			assert.Equal(t, "2021-07-11T00:00:00Z", r.DateAfter.GetValue())
 			assert.Equal(t, "2021-07-11T23:59:59Z", r.DateBefore.GetValue())
 			assert.Equal(t, "date_asc", r.Sort.GetValue())
@@ -224,7 +181,39 @@ func TestFetcher_ByDate(t *testing.T) {
 		}
 
 		assert.Equal(t, "fixture client error", err.Error())
-		seasonClient.AssertExpectations(t)
+		fixtureClient.AssertExpectations(t)
+	})
+
+	t.Run("returns an empty slice if fixtures returned do not match supported competitions", func(t *testing.T) {
+		t.Helper()
+
+		competitions := []uint64{12}
+		fixtureClient := new(MockFixtureClient)
+		seasonClient := new(MockSeasonClient)
+		clock := clockwork.NewFakeClock()
+
+		ctx := context.Background()
+
+		fetcher := fixture.NewFetcher(competitions, fixtureClient, seasonClient, clock)
+
+		req := mock.MatchedBy(func(r *statistico.FixtureSearchRequest) bool {
+			assert.Equal(t, "2021-07-11T00:00:00Z", r.DateAfter.GetValue())
+			assert.Equal(t, "2021-07-11T23:59:59Z", r.DateBefore.GetValue())
+			assert.Equal(t, "date_asc", r.Sort.GetValue())
+			return true
+		})
+
+		response := fixtureResponse()
+
+		fixtureClient.On("Search", ctx, req).Return(response, nil)
+
+		fixtures, err := fetcher.ByDate(ctx, time.Unix(1626008664, 0))
+
+		if err != nil {
+			t.Fatalf("Expected nil, got %s", err.Error())
+		}
+
+		assert.Equal(t, 0, len(fixtures))
 		fixtureClient.AssertExpectations(t)
 	})
 }
@@ -247,7 +236,24 @@ func seasonResponse() []*statistico.Season {
 }
 
 func fixtureResponse() []*statistico.Fixture {
-	return []*statistico.Fixture{}
+	return []*statistico.Fixture{
+		{
+			Id: 1,
+			Competition: &statistico.Competition{Id: 100},
+		},
+		{
+			Id: 2,
+			Competition: &statistico.Competition{Id: 8},
+		},
+		{
+			Id: 3,
+			Competition: &statistico.Competition{Id: 9},
+		},
+		{
+			Id: 4,
+			Competition: &statistico.Competition{Id: 200},
+		},
+	}
 }
 
 type MockFixtureClient struct {
