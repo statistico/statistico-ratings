788bc9082fc458e574c6b8b0c4ba20f98ef76212
Joe Sweeny <joesweeny84@hotmail.com>
2021-11-09 13:17:10 +0000

    Remove envoy proxy service and update league goal points

--- .docker/envoy/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Generating envoy.yaml config file..."
-cat /tmpl/envoy.yaml.tmpl | envsubst \$STATISTICO_RATINGS_HOST,\$STATISTICO_RATINGS_PORT > /etc/envoy/envoy.yaml
-
-echo "Starting Envoy..."
-/usr/local/bin/envoy -c /etc/envoy/envoy.yaml
